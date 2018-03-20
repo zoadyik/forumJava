0a3553b6a6b21fd6f1ce5e7cbce02be6b4269888
zoadyik <grinzsolo@stephack.com>
2018-03-20 11:03:14 +0700

    edit format

--- src/main/java/com/demo/dao/CommentDAO.java
+++ src/main/java/com/demo/dao/CommentDAO.java
@@ -6,9 +6,13 @@ import com.demo.model.CommentModel;
 
 public interface CommentDAO {
 	public void addComment(CommentModel c);
+
 	public void updateComment(CommentModel p);
-//	public List<CommentModel> listComments();
+
+	// public List<CommentModel> listComments();
 	public CommentModel getCommentById(int cId);
+
 	public void removeComment(int cId);
+
 	public List<CommentModel> listComments(int tId);
 }
--- src/main/java/com/demo/dao/CommentDAOImpl.java
+++ src/main/java/com/demo/dao/CommentDAOImpl.java
@@ -11,45 +11,47 @@ import org.springframework.stereotype.Repository;
 import com.demo.model.CommentModel;
 
 public class CommentDAOImpl implements CommentDAO {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(CommentDAOImpl.class);
-	
+
 	private SessionFactory sessionFactory;
-	
-	public void setSessionFactory(SessionFactory sf){
+
+	public void setSessionFactory(SessionFactory sf) {
 		this.sessionFactory = sf;
 	}
-	
+
 	@Override
 	public void addComment(CommentModel c) {
 		Session session = this.sessionFactory.getCurrentSession();
 		session.persist(c);
-		logger.info("Comment saved successfully, Comment Details="+c);
+		logger.info("Comment saved successfully, Comment Details=" + c);
 	}
 
 	@Override
 	public void updateComment(CommentModel p) {
 		Session session = this.sessionFactory.getCurrentSession();
 		session.update(p);
-		logger.info("Comment updated successfully, Comment Details="+p);
+		logger.info("Comment updated successfully, Comment Details=" + p);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<CommentModel> listComments(int tId) {
 		Session session = this.sessionFactory.getCurrentSession();
-		List<CommentModel> commentsList = session.createQuery("select c from CommentModel c INNER JOIN c.topic WHERE c.tId ="+String.valueOf(tId)).list();
-		for(CommentModel c : commentsList){
-			logger.info("Comment List::"+c);
+		List<CommentModel> commentsList = session
+				.createQuery("select c from CommentModel c INNER JOIN c.topic WHERE c.tId =" + String.valueOf(tId))
+				.list();
+		for (CommentModel c : commentsList) {
+			logger.info("Comment List::" + c);
 		}
 		return commentsList;
 	}
 
 	@Override
 	public CommentModel getCommentById(int cId) {
-		Session session = this.sessionFactory.getCurrentSession();		
+		Session session = this.sessionFactory.getCurrentSession();
 		CommentModel c = (CommentModel) session.load(CommentModel.class, new Integer(cId));
-		logger.info("Comment loaded successfully, Comment details="+c);
+		logger.info("Comment loaded successfully, Comment details=" + c);
 		return c;
 	}
 
@@ -57,9 +59,9 @@ public class CommentDAOImpl implements CommentDAO {
 	public void removeComment(int cId) {
 		Session session = this.sessionFactory.getCurrentSession();
 		CommentModel c = (CommentModel) session.load(CommentModel.class, new Integer(cId));
-		if(null != c){
+		if (null != c) {
 			session.delete(c);
 		}
-		logger.info("Comment deleted successfully, Comment details="+c);
+		logger.info("Comment deleted successfully, Comment details=" + c);
 	}
 }
